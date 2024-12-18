d56047f257683e23bf811910ba2c42543a0fe367
Raj <raj@mind-roots.com>
2024-12-18 17:22:45 +0530

    Update ALLOWED_HOSTS to include production domain

--- onelink/settings.py
+++ onelink/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-$(+m)0!36h^ndsu7*eog$wsn3f_hm%m@@6qraigmaz%92=^*w=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["django-b0bb.onrender.com"]
 
 
 # Application definition
