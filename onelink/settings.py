e5040e65a31c9f138c2b21beb7d1a4ef3d7e5de8
Raj <raj@mind-roots.com>
2024-12-18 19:56:35 +0530

    Revert database host in settings.py from localhost to 127.0.0.1

--- onelink/settings.py
+++ onelink/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'NAME': "onelink_db",
         'USER': "root",
         'PASSWORD': "wrMHsfs#EMXTTXiVmwgH",
-        'HOST': "localhost",
+        'HOST': "127.0.0.1",
         'PORT': "3306",
     }
 }
