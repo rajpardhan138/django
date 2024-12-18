f06a3c5fa52ab3fca335d179e59a52d0a30360e4
Raj <raj@mind-roots.com>
2024-12-18 17:25:34 +0530

    Update ALLOWED_HOSTS to include localhost for local development

--- onelink/settings.py
+++ onelink/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-$(+m)0!36h^ndsu7*eog$wsn3f_hm%m@@6qraigmaz%92=^*w=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["django-b0bb.onrender.com"]
+ALLOWED_HOSTS = ["django-b0bb.onrender.com", "127.0.0.1"]
 
 
 # Application definition
